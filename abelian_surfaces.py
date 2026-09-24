d0fcfb170c4991edd892b67f714d185c2cefbb77
jamie-meows04 <jamie.meows04@gmail.com>
2026-09-24 14:26:32 +0800

    update splitting isogenies

--- abelian_surfaces.py
+++ abelian_surfaces.py
@@ -24,6 +24,16 @@ AUTHORS:
 - Jamie Low Jiaqi (2026)
 """
 
+# ****************************************************************************
+#       Copyright (C) 2026 Jamie Low Jiaqi <m230143@e.ntu.edu.sg>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
 # To be organised
 from collections.abc import Callable, Iterable
 from functools import cached_property
